954cfada9b5a73114e0abbc053e7dc3928fcd531
owilliamgoncalves <euwilliamgoncalves@gmail.com>
2022-06-22 18:58:37 -0300

    refactor: update Tasks component with store

--- src/pages/Tasks.vue
+++ src/pages/Tasks.vue
@@ -1,39 +1,57 @@
 <template>
-  <Form @save-task="saveTask" />
+  <Form />
   <div class="lista">
-    <task
-      :key="index"
-      v-for="(task, index) of taskList"
-      :task="task"
-    />
     <Box v-if="emptyList">
       <p class="has-text-centered">
         Nenhuma tarefa finalizada.
       </p>
     </Box>
+    <Box v-else class="task-header">
+      <div class="columns">
+        <div class="column is-3">
+          Projeto
+        </div>
+        <div class="column is-4">
+          Descrição da tarefa
+        </div>
+        <div class="column">
+          Tempo
+        </div>
+        <div class="column is-2">
+          Remover
+        </div>
+      </div>
+    </Box>
+    <task
+      :key="index"
+      v-for="(task, index) in tasks"
+      :task="task"
+      :index="index"
+      @remove-task="removeTask"
+    />
   </div>
 </template>
 
 <script lang="ts" setup>
-import { computed, ref } from 'vue';
+import { storeToRefs } from 'pinia';
+import { computed } from 'vue';
 import Box from '@/components/Box.vue';
 import Form from '@/components/Form.vue';
 import Task from '@/components/Task.vue';
-import ITask from '@/interfaces/ITask';
+import useStore from '@/store';
 
-const taskList = ref<ITask[]>([]);
-const emptyList = computed(() => taskList.value.length === 0);
-
-function saveTask(description: string, time: number) {
-  taskList.value.unshift({
-    description,
-    time,
-  });
-}
+const store = useStore();
+const { removeTask } = store;
+const { tasks } = storeToRefs(store);
+const emptyList = computed(() => tasks.value.length === 0);
 </script>
 
 <style>
 .lista {
   padding: 1.25rem;
+}
+.box.task-header {
+  background-color: var(--primary-color);
+  color: var(--primary-bg-dark);
 }
 </style>
