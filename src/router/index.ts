a1bb306a16e46d388bc6b50a25e5ed4cf5988f33
owilliamgoncalves <euwilliamgoncalves@gmail.com>
2022-06-22 18:59:16 -0300

    feat: add children routes to Project's route

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,6 +10,30 @@ const routes: RouteRecordRaw[] = [
     path: '/projetos',
     name: 'Projetos',
     component: () => import(/* webpackChunkName: "tasks" */ '@/pages/Projects.vue'),
+    children: [
+      {
+        path: '',
+        name: 'ListaDeProjetos',
+        component: () => import(/* webpackChunkName: "tasks" */ '@/pages/Projects/List.vue'),
+      },
+      {
+        path: 'novo',
+        name: 'NovoProjeto',
+        component: () => import(/* webpackChunkName: "tasks" */ '@/pages/Projects/Form.vue'),
+      },
+      {
+        path: ':id/editar',
+        name: 'EditarProjeto',
+        component: () => import(/* webpackChunkName: "tasks" */ '@/pages/Projects/Form.vue'),
+        props: true,
+      },
+      {
+        path: ':id/remover',
+        name: 'RemoverProjeto',
+        component: () => import(/* webpackChunkName: "tasks" */ '@/pages/Projects/Remove.vue'),
+        props: true,
+      },
+    ],
   },
 ];
 
