7049e447cef3693a7bc7b3b4100fa78fc82d5963
owilliamgoncalves <euwilliamgoncalves@gmail.com>
2022-06-21 23:06:30 -0300

    add: theme switch on sidebar

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,25 +1,90 @@
 <template>
-  <header>
-    <h1>
+  <header class="is-flex">
+    <h1 class="has-text-centered">
       <img src="../assets/logo.png" alt="Logo Alura Tracker">
     </h1>
+    <label class="theme-switch" for="theme-switch">
+      <input
+        id="theme-switch"
+        class="is-hidden"
+        type="checkbox"
+        :value="isDarkMode"
+        @change="changeTheme"
+      >
+      <span class="theme-switch__control"></span>
+    </label>
   </header>
 </template>
 
 <script lang="ts" setup>
+import { defineEmits, defineProps } from 'vue';
+
+defineProps({
+  isDarkMode: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const emit = defineEmits<{(event: 'change-theme'): void}>();
+
+function changeTheme() {
+  emit('change-theme');
+}
 </script>
 
 <style scoped>
 header {
+  position: relative;
   padding: 1rem;
   background: #0d3b66;
   width: 100%;
   height: 100vh;
+  background-color: var(--primary-bg-dark);
+  color: var(--primary-color);
 }
 @media only screen and (max-width: 768px) {
   header {
     padding: 2.5rem;
     height: auto;
   }
+}
+.theme-switch {
+  background: var(--primary-color);
+  border-radius: 2rem;
+  width: 4.5rem;
+  height: 2.5rem;
+  padding: 0.25rem;
+  position: absolute;
+  cursor: pointer;
+  bottom: 1rem;
+  right: 1rem;
+}
+
+.theme-switch__control {
+  display: block;
+  background: var(--primary-bg);
+  border-radius: 50%;
+  width: 2rem;
+  height: 2rem;
+  border: 3px dotted var(--primary-color);
+  transition: transform 0.3s;
+}
+
+.theme-switch__control::after {
+  position: absolute;
+  width: 80%;
+  height: 80%;
+  content: "";
+  background: var(--primary-color);
+  border-radius: 50%;
+  opacity: 0;
+}
+#theme-switch:checked + .theme-switch__control {
+    transform: translateX(100%);
+    border: none;
+}
+#theme-switch:checked + .theme-switch__control::after {
+  opacity: 1;
 }
 </style>
